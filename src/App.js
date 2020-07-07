e4e5ef331c92ec0c74eab21468e86802b334c95d
nktoronto <nusrat_khwaja@hotmail.com>
2020-07-06 22:53:15 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "fontsource-roboto";
+import NumberFormat from "react-number-format";
+import React from "react";
+import "./App.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import {
+  Button,
+  Icon,
+  makeStyles,
+  Grid,
+  IconButton,
+  AppBar,
+  Toolbar,
+  Typography,
+  paper,
+} from "@material-ui/core";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <h1> Hello World </h1>
+    
     </div>
-  );
+    );
 }
 
 export default App;
